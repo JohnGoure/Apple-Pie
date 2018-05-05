6b916c562dae219dc5db6e0682233851469ad883
JohnGoure <johnsgithub@gmail.com>
2018-05-05 10:56:34 -0500

    Implemented a little bit of the game logic. Made a Game struct that represents the games state. Implemented 2 methods in the view controller, newRound and updateUi. They do what their name implies.

---
+++ ApplePie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by john goure on 5/5/18.
+//  Copyright © 2018 john goure. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listOfWords = ["eggs", "chicken", "bacon", "sausage", "bread", "basketball", "baseball", "teading", "golf", "dog", "cat", "sponge", "soap"]
+    
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    var totalLosses = 0
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
-
+    
+    var currentGame: Game!
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
